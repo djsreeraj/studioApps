9a88979f26fae284e1d60f9cee579e78e1d7cb07
djsreeraj <sreeraj011m@gmail.com>
2016-08-11 21:16:53 +0530

    Initial commit

---
+++ app/src/main/java/com/example/android/mysqldemo/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.android.mysqldemo;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    EditText usernameEt,passwordEt;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        usernameEt = (EditText) findViewById(R.id.etUsername);
+        passwordEt = (EditText) findViewById(R.id.etPassword);
+    }
+
+    public void OnLogin(View view){
+        String username = usernameEt.getText().toString();
+        String password = passwordEt.getText().toString();
+        String type = "login";
+
+        BackgroundWorker backgroundWorker = new BackgroundWorker(this);
+        backgroundWorker.execute(type, username, password);
+    }
+
+}
